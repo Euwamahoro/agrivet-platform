cbf905115b1d27acc19a34a30c9ad0562a26bda5
Euwamahoro <e.uwamahoro@alustudent.com>
2025-11-23 21:23:48 +0200

    main menu number fixing

--- agrivet_ussd/src/services/ussdService.js
+++ agrivet_ussd/src/services/ussdService.js
@@ -128,7 +128,6 @@ const getDynamicMainMenu = async (locale, phoneNumber) => {
     `${mainMenuItem1}\n` +                           // 1. Register/Update
     `${getTranslatedMessage('menu_option_request_service', locale)}\n` +
     `${getTranslatedMessage('menu_option_farming_tips', locale)}\n` +
-     `${getTranslatedMessage('menu_option_weather_info', locale)}\n` +  
     `${getTranslatedMessage('menu_option_my_request_status', locale)}\n` + 
     `${getTranslatedMessage('menu_option_change_language', locale)}\n` + 
     `${getTranslatedMessage('menu_option_exit', locale)}`;
