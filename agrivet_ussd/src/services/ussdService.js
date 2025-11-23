972f29f73e771f4c99f63d03108d08f8d57412a9
Euwamahoro <e.uwamahoro@alustudent.com>
2025-11-23 18:49:29 +0200

    better main menu

--- agrivet_ussd/src/services/ussdService.js
+++ agrivet_ussd/src/services/ussdService.js
@@ -61,7 +61,6 @@ const getLanguageSelectionMenu = () => {
   return message;
 };
 
-// Move these functions outside of getDynamicMainMenu
 
 const getServiceTypeMenu = (locale) => {
   const message = getTranslatedMessage('prompt_service_type', locale);
@@ -97,13 +96,13 @@ const getDynamicMainMenu = async (locale, phoneNumber) => {
 
   const message =
     `${getTranslatedMessage('main_menu_welcome', locale)}\n` +
-    `${mainMenuItem1}\n` +
-    `${getTranslatedMessage('menu_option_request_service', locale)}\n` +
-    `${getTranslatedMessage('menu_option_weather_info', locale)}\n` +
-    `${getTranslatedMessage('menu_option_farming_tips', locale)}\n` +
-    `${getTranslatedMessage('menu_option_my_request_status', locale)}\n` +
-    `${getTranslatedMessage('menu_option_change_language', locale)}\n` +
-    `${getTranslatedMessage('menu_option_exit', locale)}`;
+    `${mainMenuItem1}\n` +                           // 1. Register/Update
+    `${getTranslatedMessage('menu_option_request_service', locale)}\n` +     // 2. Request Service
+    `${getTranslatedMessage('menu_option_my_request_status', locale)}\n` +   // 3. My Request Status
+    `${getTranslatedMessage('menu_option_weather_info', locale)}\n` +        // 4. Weather Info
+    `${getTranslatedMessage('menu_option_farming_tips', locale)}\n` +        // 5. Farming Tips
+    `${getTranslatedMessage('menu_option_change_language', locale)}\n` +     // 6. Change Language
+    `${getTranslatedMessage('menu_option_exit', locale)}`;                   // 7. Exit
   return message;
 };
 
